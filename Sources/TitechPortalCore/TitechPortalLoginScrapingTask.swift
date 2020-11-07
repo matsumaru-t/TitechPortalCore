229e75e3322332dd16b829911530c3b00ad5b6bb
napier <m-t2000@outlook.jp>
2020-11-07 15:33:02 +0900

    少し整形

--- Sources/TitechPortalCore/TitechPortalLoginScrapingTask.swift
+++ Sources/TitechPortalCore/TitechPortalLoginScrapingTask.swift
@@ -10,21 +10,22 @@ import Foundation
 public struct TitechPortalLoginScrapingTask {
     public static func login() {
         print("login")
+        let loginURL = URL(string: "https://portal.nap.gsic.titech.ac.jp/GetAccess/Login?Template=userpass_key&AUTHMETHOD=UserPassword")!
+        let postURL = URL(string: "https://portal.nap.gsic.titech.ac.jp/GetAccess/Login")!
+        
         URLSession.shared
-            .dataTask(with: URL(string: "https://portal.nap.gsic.titech.ac.jp/GetAccess/Login?Template=userpass_key&AUTHMETHOD=UserPassword")!) { data, response, error in
-//                print(response)
-//                print(String(data: data!, encoding: .utf8))
+            .dataTask(with: loginURL) { data, response, error in
                 let allowedCharacterSet = CharacterSet(charactersIn: "!'();:@&=+$,/?%#[]").inverted
                 let inputs = HTMLInputParser.parse(data!).map {
-                        var value: String
-                        
-                        switch $0.name {
-                        case "usr_name":
-                            value = MyData.usrName
-                        case "usr_password":
-                            value = MyData.usrPassword
-                        default:
-                            value = $0.value
+                    var value: String
+                    
+                    switch $0.name {
+                    case "usr_name":
+                        value = MyData.usrName
+                    case "usr_password":
+                        value = MyData.usrPassword
+                    default:
+                        value = $0.value
                     }
             
                     let encodedStr = value.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet)?.replacingOccurrences(of: " ", with: "+") ?? ""
@@ -32,27 +33,25 @@ public struct TitechPortalLoginScrapingTask {
                     return "\($0.name)=\(encodedStr)"
                 }.joined(separator: "&")
                 
-//                print(inputs.replacingOccurrences(of: MyData.usrPassword, with: "aaaaaaaa"))
-                
-                var urlRequest = URLRequest(url: URL(string: "https://portal.nap.gsic.titech.ac.jp/GetAccess/Login")!)
+                var urlRequest = URLRequest(url: postURL)
                 urlRequest.httpMethod = "POST"
                 urlRequest.httpBody = inputs.data(using: .utf8)
-                urlRequest.setValue("https://portal.nap.gsic.titech.ac.jp/GetAccess/Login?Template=userpass_key&AUTHMETHOD=UserPassword", forHTTPHeaderField: "Referer")
+                urlRequest.setValue(postURL.absoluteString, forHTTPHeaderField: "Referer")
                 
                 URLSession.shared.dataTask(with: urlRequest) { data, response, error in
                     let keys = MatrixKeyParser.parse(data!)
                     let inputs = HTMLInputParser.parse(data!).map {
-                            var value: String
+                        var value: String
 
-                            switch $0.name {
-                            case "message3":
-                                value = MyData.matrix[keys[0]] ?? ""
-                            case "message4":
-                                value = MyData.matrix[keys[1]] ?? ""
-                            case "message5":
-                                value = MyData.matrix[keys[2]] ?? ""
-                            default:
-                                value = $0.value
+                        switch $0.name {
+                        case "message3":
+                            value = MyData.matrix[keys[0]] ?? ""
+                        case "message4":
+                            value = MyData.matrix[keys[1]] ?? ""
+                        case "message5":
+                            value = MyData.matrix[keys[2]] ?? ""
+                        default:
+                            value = $0.value
                         }
 
                         let encodedStr = value.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet)?.replacingOccurrences(of: " ", with: "+") ?? ""
@@ -60,10 +59,7 @@ public struct TitechPortalLoginScrapingTask {
                         return "\($0.name)=\(encodedStr)"
                     }.joined(separator: "&")
                     
-                    var urlRequest = URLRequest(url: URL(string: "https://portal.nap.gsic.titech.ac.jp/GetAccess/Login")!)
-                    urlRequest.httpMethod = "POST"
                     urlRequest.httpBody = inputs.data(using: .utf8)
-                    urlRequest.setValue("https://portal.nap.gsic.titech.ac.jp/GetAccess/Login?Template=userpass_key&AUTHMETHOD=UserPassword", forHTTPHeaderField: "Referer")
                     
                     URLSession.shared.dataTask(with: urlRequest) { data, response, error in
                         print(String(data: data!, encoding: .utf8)!)
